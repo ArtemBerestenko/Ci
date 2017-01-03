5962ff59d7bebc8c8409e62a80c65ea4b1c1b5f3
aberestenko <ju1ian@bigmir.net>
2017-01-03 17:31:57 +0200

    primitive project added

---
+++ src/main/java/GooglePage.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+/**
+ * Created by aberestenko on 12/28/2016.
+ */
+public class GooglePage {
+    String url;
+    WebDriver driver;
+    private By googleInputLocator = By.id("lst-ib");
+
+    public GooglePage() {
+        System.setProperty("webdriver.gecko.driver", "src\\test\\resources\\geckodriver.exe");
+        driver = new FirefoxDriver();
+
+        url = "https://www.google.com.ua";
+        driver.get(url);
+    }
+
+
+    public void setTextToSearch(String text) {
+        driver.findElement(googleInputLocator).sendKeys(text);
+        driver.findElement(googleInputLocator).sendKeys(Keys.ENTER);
+
+    }
+
+
+}
---
+++ src/test/java/GooglePageTest.java
@@ -0,0 +1,16 @@
+/**
+ * Created by aberestenko on 1/3/2017.
+ */
+
+
+import org.junit.Test;
+
+public class GooglePageTest {
+    @Test
+    public void openGooglePageAndSetSomeText() {
+        GooglePage googlePage = new GooglePage();
+        googlePage.setTextToSearch("Book in Ukraine");
+
+    }
+
+}
